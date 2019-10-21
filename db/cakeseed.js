d95b0cf401f268ae471d7e63407a5c4e12c16798
Brian Loveless <brianloveless@Brians-Air.hsd1.ga.comcast.net>
2019-10-21 11:45:54 -0400

    omg

--- db/cakeseed.js
+++ db/cakeseed.js
@@ -1,68 +1,17 @@
-[{
-        "title": "Pound Cake",
-        "category": "Cake",
-        "description": "Paula Dean Delicousness",
-        "items": [
-            "1/2 pound butter",
-            "1/2 cup veg shortening",
-            "3 cups sugar",
-            "5 eggs",
-            "3 cups all purpose flour",
-            "1/2 teaspoon salt",
-            "1/2 teaspoon baking powder",
-            "1 cup milk",
-            "1 teaspoon vanilla extract"
-        ],
-        "steps": [
-            "1. preheat oven to 350 deg f",
-            "in mixer cream butter and shortening",
-            "add eggs one at a time",
-            "stir dry ingredients in a seperate bowl",
-            "add dry ingredients and milk alternately to mixer",
-            "start and end with flour",
-            "add vanilla",
-            "pour into greased tube pan",
-            "bake for 65-95 minutes or until toothpick comes out clean from middle"
-        ],
-        "image": "https://images-gmi-pmc.edge-generalmills.com/82b53a9e-a711-4697-957c-81a007c4dbd3.jpg"
-    },
+const Cakes = require('');
+const cake = require('');
 
-    {
-        "title": "Pound Cake Old School",
-        "category": "Cake",
-        "description": "Old School Style",
-        "items": [
-            "1 pound butter",
-            "1 pound sugar",
-            "5 eggs",
-            "1 pound all purpose flour"
-        ],
-        "steps": [
-            "1. preheat oven to 350 deg f",
-            "2 . mix ingredients",
-            "3. pour into greased tube pan",
-            "bake for 65-95 minutes or until toothpick comes out clean from middle"
-        ],
-        "image": "https://revolutionarypie.files.wordpress.com/2013/05/img_1491.jpg"
-    },
-    {
-        "title": "Cherry Pineapple Dump Cake",
-        "category": "Cake",
-        "description": "Easy and Tasty",
-        "items": [
-            "21 oz can cherry pie filling",
-            "15 oz can crushed pineapple",
-            "1 box white cake mix (18oz)",
-            "1 1/2 sticks b utter (12 tblspns)"
-        ],
-        "steps": [
-            "1. Preheat oven to 350",
-            "2. Dump cherry pie filling and crushed pineapple into 13 x 9 baking dish stir together",
-            "3. Sprinkle cake mix over top of fruit",
-            "4. Slice butter sticks inbto small chunks and distrubute evenly over top of cake mix ontop of fruit ",
-            "5. Bake until top is brown and bubbly (45 minutes to an hour)",
-            "6. serve with whipped cream"
-        ],
-        "image": "https://www.gonnawantseconds.com/wp-content/uploads/2019/12/Cherry-Pineapple-Dump-Cake-7-2.jpg"
-    }
-]
+
+// clear the database of all cheerups insert original seeds
+
+
+Cakes.deleteMany({})
+    .then(() => {
+        console.log('old cheers gone sir');
+        return Cakes.insertMany(cake);
+        // create cheerup files from seeds
+    })
+    .then(() => {
+        console.log(' ㋡ cake recpz 🎂 back to originals 💕');
+        process.exit();
+    });
