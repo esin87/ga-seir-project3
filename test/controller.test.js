ea967348d06ef16fd608dd1363e58edc577f9bb0
Brian Loveless <Brian.loveless1@yahoo.com>
2019-10-25 09:59:47 -0400

    testing fluff

--- test/controller.test.js
+++ test/controller.test.js
@@ -1,36 +1,46 @@
-const should = require("chai").should();
-const expect = require("chai").expect;
-const supertest = require("supertest");
-const api = supertest("http://localhost:3000");
+const should = require('chai').should();
+const expect = require('chai').expect;
+const supertest = require('supertest');
+const api = supertest('http://localhost:3000');
 
 /// TESTING
 
 //////////////////////////////////////////////////////
 /// tests for crud functions
-// cd into db in terminal and node cakeseed.js
+//  cd into db in terminal and node cakeseed.js
 //  each time before running tests to reset db data to original.
-/// then check id's cause the flippin change each time you re-seed
+/// then check id's cause they flippin change each time you re-seed
 //////////////////////////////////////////////////////
 
 /// checking r of crud - read
 ///router.get
 //  deep r of crud - read with attributes
 
-// describe("GET /titles/:title", () => {
-//     it("should return a dessert by name (title) with correct fields", done => {
-//         api
-//             .get("/titles/EZ-Brownies")
-//             .set("Accept", "application/json")
-//             .end((error, response) => {
-//                 console.log(response.body.length);
-//                 expect(response.body).to.have.property("_id");
-//                 expect(response.body).to.have.property("title");
-//                 expect(response.body).to.have.property("category");
-//                 expect(response.body).to.have.property("description");
-//                 done();
-//             });
-//     });
-// });
+describe('GET /titles/:title', () => {
+	before(done => {
+		api
+			.get('/titles/Sugar%20Cookies')
+			.set('Accept', 'application/json')
+			.end(done);
+	});
+
+	it('should return a dessert by name (title) with correct fields', done => {
+		api
+			.get('/titles/Sugar%20Cookies')
+			.set('Accept', 'application/json')
+			.end((error, response) => {
+				console.log(response.body);
+				expect(response.body).to.have.property('_id');
+				expect(response.body).to.have.property('title');
+				expect(response.body).to.have.property('category');
+				expect(response.body).to.have.property('description');
+				done();
+			});
+	});
+});
+
+/// until we figure out how to go and get the id back from a get request
+// I'll have to hardcode for testing - below works but commented out since re-seed = new id's
 
 //// checking c of crud - create - we have 14 files in our seed data - 2 per category
 ///router.post - works fine
@@ -73,6 +83,13 @@ const api = supertest("http://localhost:3000");
 //             .set("Accept", "application/json")
 //             .end((error, response) => {
 //                 console.log(response.body);
+//                  console.log(response.body.title);
+//                  console.log(response.body.property.title);
+//                  console.log(response.body.info.title);
+//                  console.log(response.body.[0].title);
+//                  console.log(response.body.[0].info.title);
+//                  console.log(response.body.[0].property.title);
+//                  console.log(response.body.info.property.title);
 //                 expect(response.body).to.have.property("_id");
 //                 expect(response.body).to.have.property("title");
 //                 expect(response.body).to.have.property("category");
@@ -91,7 +108,7 @@ const api = supertest("http://localhost:3000");
 /// checking d of crud - delete it ... omg that 📷 is sooo baddd deelllete iiitttttt !!
 //router.delete by title
 
-///
+//
 //   { items: [ 'brownie mix box', '1 egg', 'water', 'oil' ],
 //   steps: [ '1. Preheat oven to 350', '2. follow box directions' ],
 //   _id: '5daf2748aaeb3854dcc24dd8',
@@ -123,51 +140,51 @@ const api = supertest("http://localhost:3000");
 
 //// check get by category return 2 pies since only 2 in seed data
 
-describe("GET /categories/:category", () => {
-    it("should return our pie desserts length which is 2", done => {
-        api
-            .get("/categories/Pies")
-            .set("Accept", "application/json")
-            .end((error, response) => {
-                console.log("check here for pie length");
-                console.log(response.body.length);
-                expect(response.body.length).to.equal(2);
-                done();
-            });
-    });
-});
+// describe('GET /categories/:category', () => {
+// 	it('should return our pie desserts length which is 2', done => {
+// 		api
+// 			.get('/categories/Pies')
+// 			.set('Accept', 'application/json')
+// 			.end((error, response) => {
+// 				console.log('check here for pie length');
+// 				console.log(response.body.length);
+//                 console.log(response.body.title);
+//                 console.log(response.body)
+// 				let foo = response.body.title;
+// 				expect(response.body.length).to.equal(2);
+// 				console.log(foo);
+// 				done();
+// 			});
+// 	});
+// });
 
 // "_id": "5db0b4559e47abb20a5b453d",
 // "title": "Paula Dean Pound Cake",
 // "category": "Cakes",
 
-
-
 ///// test new routes
-// show by ID - works via local host in chrome - 
-//but gives 2000 ms timeout error when testing ...
-/// when reseediong to remove the created one - new id's made by mongo / mongoose - 
+// show by ID - works via local host in chrome -
+//but gives 2000 ms timeout error when testing ... id not found
+/// when reseediong to remove the created one - new id's made by mongo / mongoose -
 ///how to do by specific id since generated each time
-/// stop reseeding and comment out create - it works - 
+/// stop reseeding and comment out create - it works -
 
+// describe("GET /dessert/:id", () => {
+//     it("should return paula pound cake by id in case of same titles", done => {
+//         api
+//             .get("/dessert/5db0b4559e47abb20a5b453d")
+//             .set("Accept", "application/json")
+//             .end((error, response) => {
+//                 console.log(response.body.title);
+//                 expect(response.body).to.have.property("title");
+//                 expect(response.body).to.have.property("category");
+//                 expect(response.body).to.have.property("description");
+//                 done();
+//             });
+//     });
+// });
 
-describe("GET /dessert/:id", () => {
-    it("should return paula pound cake by id in case of same titles", done => {
-        api
-            .get("/dessert/5db0b4559e47abb20a5b453d")
-            .set("Accept", "application/json")
-            .end((error, response) => {
-                console.log(response.body.title);
-                expect(response.body).to.have.property("title");
-                expect(response.body).to.have.property("category");
-                expect(response.body).to.have.property("description");
-                done();
-            });
-    });
-});
-
-
-// "_id": "5db0b09c97cbd7b17e7d03e9", - was working 
+// "_id": "5db0b09c97cbd7b17e7d03e9", - was working
 // "title": "Whipped Pie",
 // "category": "Pies",
 // "description": "great for faces and clowns",
@@ -176,49 +193,44 @@ describe("GET /dessert/:id", () => {
 // "title": "Whipped Pie",
 // "category": "Pies",
 
-
-
-describe("GET /dessert/:id", () => {
-    it("should return whip pie  by id in case of same titles", done => {
-        api
-            .get("/dessert/5db0b4559e47abb20a5b453f")
-            .set("Accept", "application/json")
-            .end((error, response) => {
-                console.log(response.body.title);
-                expect(response.body).to.have.property("title");
-                expect(response.body).to.have.property("category");
-                expect(response.body).to.have.property("description");
-                done();
-            });
-    });
-});
-
-
+// describe("GET /dessert/:id", () => {
+//     it("should return whip pie  by id in case of same titles", done => {
+//         api
+//             .get("/dessert/5db0b4559e47abb20a5b453f")
+//             .set("Accept", "application/json")
+//             .end((error, response) => {
+//                 console.log(response.body.title);
+//                 expect(response.body).to.have.property("title");
+//                 expect(response.body).to.have.property("category");
+//                 expect(response.body).to.have.property("description");
+//                 done();
+//             });
+//     });
+// });
 
 // delete by id
-/// ids change when re-seed db - this should work thius time
-
+/// ids change when re-seed db - this should work this time - need to learn the syntax for going and looking up
 
 // "_id": "5db0b4559e47abb20a5b4543",
 // "title": "chocChip",
 // "category": "Cookies",
 
-describe("DELETE by flippin /:id", () => {
-    before(done => {
-        api
-            .delete("/5db0b4559e47abb20a5b4543")
-            .set("Accept", "application/json")
-            .end(done);
-    });
-    it("should remove the old cookie object by id =( 5db0b4559e47abb20a5b4543 ) from the dessert db ", done => {
-        api
-            .get("/")
-            .set("Accept", "application/json")
-            .end((error, response) => {
-                console.log(response.body.length);
-                expect(response.body.length).to.equal(13);
-                // one less than above with add - change to 13 if create test commented out
-                done();
-            });
-    });
-});
+// describe("DELETE by flippin /:id", () => {
+//     before(done => {
+//         api
+//             .delete("/5db0b4559e47abb20a5b4543")
+//             .set("Accept", "application/json")
+//             .end(done);
+//     });
+//     it("should remove the old cookie object by id =( 5db0b4559e47abb20a5b4543 ) from the dessert db ", done => {
+//         api
+//             .get("/")
+//             .set("Accept", "application/json")
+//             .end((error, response) => {
+//                 console.log(response.body.length);
+//                 expect(response.body.length).to.equal(13);
+//                 // one less than above with add - change to 13 if create test commented out
+//                 done();
+//             });
+//     });
+// });
